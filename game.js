c555b90e99ff372bd173125ccaba305c67569395
ndow33 <nodw33@gmail.com>
2021-12-06 11:55:36 -0700

    added blink function

--- game.js
+++ game.js
@@ -1,14 +1,28 @@
+
+// console.log("Hello World");
+//Variables
 var gamePattern = [];
-
-var buttonColors = ['red','yellow','blue','green'];
-
+var buttonColors = ['red','blue','green','yellow'];
 var randomChosenColor = buttonColors[nextSequence()];
+var blueSound = new Audio('sounds/blue.mp3');
 
+//functions
+//generates a random color for the sequence
 function nextSequence(){
     var randomNumber = Math.floor(Math.random()*4)
     return randomNumber
 };
+//Blink function
+function blink(color){
+    $(("#" + color)).animate({opacity: "0.2"}, 1000);
+    $(("#" + color)).animate({opacity:"1"}, 1000);
+    blueSound.play();
+};
 
+//Add the new color to the game pattern list
 gamePattern.push(randomChosenColor)
+blink(randomChosenColor);
 
+//Console logs
 console.log(gamePattern)
+console.log($(("#" + randomChosenColor)));
