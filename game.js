696bee7fb7cb9a92e88f3b126c24034c250056a4
ndow33 <nodw33@gmail.com>
2021-12-06 16:01:49 -0700

    progress made on using sounds

--- game.js
+++ game.js
@@ -2,9 +2,9 @@
 // console.log("Hello World");
 //Variables
 var gamePattern = [];
+var userClickedPatterns = [];
 var buttonColors = ['red','blue','green','yellow'];
 var randomChosenColor = buttonColors[nextSequence()];
-var blueSound = new Audio('sounds/blue.mp3');
 
 //functions
 //generates a random color for the sequence
@@ -14,10 +14,28 @@ function nextSequence(){
 };
 //Blink function
 function blink(color){
-    $(("#" + color)).animate({opacity: "0.2"}, 1000);
-    $(("#" + color)).animate({opacity:"1"}, 1000);
-    blueSound.play();
+    $(("#" + color)).animate({opacity: "0.2"}, 200);
+    $(("#" + color)).animate({opacity:"1"}, 200);
 };
+//play the sound associated with the chosen color
+function playSound(color){
+    var newColorSound = new Audio('sounds/'+color+'.mp3');
+    newColorSound.play();
+}
+
+//Event listeners
+//Which button is clicked?
+$("button").click(function(){
+    //Get the id of the clicked button
+    var chosenColor = $(this).attr("id");
+    //plug the id into our functions
+    blink(chosenColor);
+    playSound(chosenColor);
+    //add the chosen color to userClickedPatterns
+    userClickedPatterns.push(chosenColor);
+    console.log(userClickedPatterns)
+})
+
 
 //Add the new color to the game pattern list
 gamePattern.push(randomChosenColor)
